64d2c9a8c766d9a2bdaf5bc45d7f0c78257ee087
Jocelyn <128290009+Lonejyc@users.noreply.github.com>
2024-03-12 19:01:12 +0100

    Button not working

--- src/components/Wheel.jsx
+++ src/components/Wheel.jsx
@@ -4,13 +4,60 @@ import * as d3 from 'd3';
 import { scaleOrdinal } from 'd3-scale';
 import "./Wheel.css";
 
-function Wheel() {
-    // const [items, setItems] = useState([
-    //   { label: 'Test', value: 1 }
-    // ]);
-  
-    // const [newItems, setNewItems] = useState("");
-    
+let oldrotation = 0;
+let rotation = 0;
+let picked = 0;
+const oldpick = [];
+
+function spin(el) {
+  el.on('click', null);
+
+  if (oldpick.length === data.length) {
+    console.log('done');
+    el.on('click', null);
+    return;
+  }
+
+  const ps = 360 / data.length;
+  const pieslice = Math.round(1440 / data.length);
+  const rng = Math.floor(Math.random() * 1440 + 360);
+
+  rotation = Math.round(rng / ps) * ps;
+
+  picked = Math.round(data.length - (rotation % 360) / ps);
+  picked = picked >= data.length ? picked % data.length : picked;
+
+  if (oldpick.indexOf(picked) !== -1) {
+    spin();
+    return;
+  } else {
+    oldpick.push(picked);
+  }
+
+  rotation += 90 - Math.round(ps / 2);
+
+  vis
+    .transition()
+    .duration(3000)
+    .attrTween('transform', rotTween)
+    .each('end', function () {
+      d3.select('.slice:nth-child(' + (picked + 1) + ') path').attr('fill', '#111');
+      oldrotation = rotation;
+
+      console.log(data[picked].value);
+
+      el.on('click', spin);
+    });
+}
+
+function rotTween(to) {
+  const i = d3.interpolate(oldrotation % 360, rotation);
+  return function (t) {
+    return 'rotate(' + i(t) + ')';
+  };
+}
+
+function Wheel() {    
     const [data, setData] = useState([
       { label: 'Test', value: 1 },
       { label: 'Test', value: 2 },
@@ -18,18 +65,6 @@ function Wheel() {
     ]);
     
     const [newDatas, setNewDatas] = useState("");
-  
-    // comportements
-    // const handleDelete = (value) => {
-    //   // 1. copie du state
-    //   const itemsCopy = [...items];
-  
-    //   // 2. manipulation sur la copie du state
-    //   const itemsCopyUpdated = itemsCopy.filter((item) => item.value !== value);
-  
-    //   // 3. modifier le state avec le setter
-    //   setItems(itemsCopyUpdated);
-    // };
 
     const handleDelete = (value) => {
       // 1. copie du state
@@ -41,23 +76,6 @@ function Wheel() {
       // 3. modifier le state avec le setter
       setData(datasCopyUpdated);
     };
-  
-    // const handleSubmit = (event) => {
-    //   event.preventDefault();
-  
-    //   // 1. copie du state
-    //   const itemsCopy = [...items];
-  
-    //   // 2. manipulation sur la copie du state
-    //   const value = new Date().getTime();
-    //   const label = newItems;
-    //   const itemToAdd = { value, label };
-    //   itemsCopy.push(itemToAdd);
-  
-    //   // 3. modifier le state avec le setter puis vider l'input
-    //   setItems(itemsCopy);
-    //   setNewItems("");
-    // };
 
     const handleSubmit = (event) => {
       event.preventDefault();
@@ -80,19 +98,22 @@ function Wheel() {
       setNewDatas(event.target.value);
     };
 
+    function handleSpinClick(el) {
+      spin(el);
+    }
+
   const chartRef = useRef(null);
 
   useEffect(() => {
-    const padding = { top: 20, right: 40, bottom: 0, left: 0 };
+    const padding = { top: 10, right: 20, bottom: 10, left: 20 };
     const w = 500 - padding.left - padding.right;
     const h = 500 - padding.top - padding.bottom;
     const r = Math.min(w, h) / 3;
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
+    // const color = d3.scaleOrdinal(d3.schemeCategory10);
+    const color = ['#f54242', '#4287f5', '#f5bf42', '#42f56f', '#f5d742', '#c242f5'];
+    const svg = d3.select('svg');
 
-    const svg = d3
-      .select(chartRef.current)
-      .select('svg')
-      .append('svg');
+    svg.selectAll('*').remove();
 
     const container = svg
       .append('g')
@@ -120,7 +141,10 @@ function Wheel() {
     arcs
       .append('path')
       .attr('fill', function (d, i) {
-        return color(i);
+        if (color[i % color.length] === undefined) {
+          color[i % color.length] -= color.length;
+        }
+        return color[i % color.length];
       })
       .attr('d', function (d) {
         return arc(d);
@@ -145,60 +169,8 @@ function Wheel() {
         return data[i].label;
       });
 
-    container.on('click', spin);
+    // container.on('click', spin);
 
-    let oldrotation = 0;
-    let rotation = 0;
-    let picked = 0;
-    const oldpick = [];
-
-    function spin(d) {
-      container.on('click', null);
-
-      if (oldpick.length === data.length) {
-        console.log('done');
-        container.on('click', null);
-        return;
-      }
-
-      const ps = 360 / data.length;
-      const pieslice = Math.round(1440 / data.length);
-      const rng = Math.floor(Math.random() * 1440 + 360);
-
-      rotation = Math.round(rng / ps) * ps;
-
-      picked = Math.round(data.length - (rotation % 360) / ps);
-      picked = picked >= data.length ? picked % data.length : picked;
-
-      if (oldpick.indexOf(picked) !== -1) {
-        spin();
-        return;
-      } else {
-        oldpick.push(picked);
-      }
-
-      rotation += 90 - Math.round(ps / 2);
-
-      vis
-        .transition()
-        .duration(3000)
-        .attrTween('transform', rotTween)
-        .each('end', function () {
-          d3.select('.slice:nth-child(' + (picked + 1) + ') path').attr('fill', '#111');
-          oldrotation = rotation;
-
-          console.log(data[picked].value);
-
-          container.on('click', spin);
-        });
-    }
-
-    function rotTween(to) {
-      const i = d3.interpolate(oldrotation % 360, rotation);
-      return function (t) {
-        return 'rotate(' + i(t) + ')';
-      };
-    }
   }, [data]);
 
 //affichage (render)
@@ -206,7 +178,9 @@ function Wheel() {
   return (
       <div className="winwheel">
         <div id="chart" ref={chartRef}>
-          <svg width="500" height="500"></svg>
+          <svg className="svg_" width="500" height="500"></svg>
+          <button className="spin" onClick={(event) => handleSpinClick(event.target)}>SPIN</button>
+          <div className="targeter"></div>
         </div>
         <ul>
           {data.map((data) => (
